613feccf2838ca5d4527b38cd8861257230494fc
kahvia-d <xuedongtan@163.com>
2022-12-25 01:50:53 +0800

    为卡片添加了背面。为卡片添加了旋转动画。背面待完善。修复了卡片背面覆盖正面导致无法与正面交互的bug（通过动态设置visibility解决）。

--- app/src/main/java/cn/kahvia/adoing/PagesActivity.java
+++ app/src/main/java/cn/kahvia/adoing/PagesActivity.java
@@ -4,6 +4,9 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -11,8 +14,10 @@ import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,11 +31,14 @@ public class PagesActivity extends AppCompatActivity {
     private ViewPager2 viewPager2;
     private List<CardItem> cardItems;
     public static TextView cardCounter;
+    private AnimatorSet frontAniSet;
+    private AnimatorSet backAniSet;
     private BroadcastReceiver myBroadcastReceiver=new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             if (intent!=null){
                 int time=intent.getIntExtra("Counter",0);
+                //根据广播内容，更新UI
                 cardCounter.setText(TimeUtil.formatTime(time));
             }
 
@@ -39,6 +47,9 @@ public class PagesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        frontAniSet= (AnimatorSet) AnimatorInflater.loadAnimator(this,R.animator.front_anim);
+        backAniSet= (AnimatorSet) AnimatorInflater.loadAnimator(this,R.animator.back_anim);
 
         setContentView(R.layout.activity_pages);
 
@@ -59,35 +70,53 @@ public class PagesActivity extends AppCompatActivity {
         viewPager2.setAdapter(new ViewPagerAdaptor(cardItems));
         viewPager2.setClipToPadding(false);
         viewPager2.setClipChildren(false);
-        viewPager2.setOffscreenPageLimit(1);
+        viewPager2.setOffscreenPageLimit(2);
         viewPager2.setCurrentItem(CounterService.pageIndex,false);
     }
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void onCardClickStart(View view){
-        //获取父节点，即当前卡片，在其下寻找相应的组件
+        if (!CounterService.running){
+            //计时器服务，线程停止后可开启新的计时任务
+            if (CounterService.threadEnd){
+                //获取父节点，即当前卡片，在其下寻找相应的组件
+                View parent= (View) view.getParent();
+                Button startButton=parent.findViewById(R.id.startButton);
+                TextView pastTime=parent.findViewById(R.id.pastTime);
+                TextView pageIndex=parent.findViewById(R.id.pageIndex);
 
-        View parent= (View) view.getParent();
-        Button startButton=parent.findViewById(R.id.startButton);
-        TextView pastTime=parent.findViewById(R.id.pastTime);
-        TextView pageIndex=parent.findViewById(R.id.pageIndex);
-        cardCounter=pastTime;
-        startButton.setVisibility(View.INVISIBLE);
-        pastTime.setVisibility(View.VISIBLE);
+                //保存展示计时器数据的对象，用以更新UI，显示最新计时数据
+                cardCounter=pastTime;
+                //隐藏按钮，显示计时
+                startButton.setVisibility(View.INVISIBLE);
+                pastTime.setVisibility(View.VISIBLE);
 
-        //start my counter service
-        Intent serviceIntent=new Intent(this, CounterService.class);
-        serviceIntent.setAction("start");
-        serviceIntent.putExtra("pageIndex",pageIndex.getText());
-        startForegroundService(serviceIntent);
+                //重新开始计时的时候，设定计时初始值
+                pastTime.setText(TimeUtil.formatTime(CounterService.counter));
+
+                //start my counter service
+                Intent serviceIntent=new Intent(this, CounterService.class);
+                serviceIntent.setAction("start");
+                serviceIntent.putExtra("pageIndex",pageIndex.getText());
+                startForegroundService(serviceIntent);
+            }else {
+                Toast.makeText(view.getContext(),"正在记录数据，请稍后",Toast.LENGTH_SHORT).show();
+
+            }
+        }else {
+            Toast.makeText(view.getContext(),"只能开启单任务",Toast.LENGTH_SHORT).show();
+        }
+
     }
     public void onCardClickEnd(View view){
-        //获取父节点，即当前卡片
+        //获取父节点，即当前卡片，在其下寻找相应的组件
         View parent= (View) view.getParent();
         Button startButton=parent.findViewById(R.id.startButton);
         TextView pastTime=parent.findViewById(R.id.pastTime);
         TextView pageIndex=parent.findViewById(R.id.pageIndex);
+
+        //显示按钮，隐藏计时
         startButton.setVisibility(View.VISIBLE);
         pastTime.setVisibility(View.INVISIBLE);
 
@@ -98,8 +127,41 @@ public class PagesActivity extends AppCompatActivity {
         startService(serviceIntent);
     }
 
+    public void onCardFlip(View view){
+        View cardsParent=(View) view.getParent().getParent().getParent();
+        View cardFront=cardsParent.findViewById(R.id.card_front);
+        View cardBack=cardsParent.findViewById(R.id.card_back);
+
+        frontAniSet.setTarget(cardFront);
+        backAniSet.setTarget(cardBack);
+
+        float scale=this.getResources().getDisplayMetrics().density;
+        cardFront.setCameraDistance(scale*10000);
+        cardBack.setCameraDistance(scale*10000);
+
+        cardBack.setVisibility(View.VISIBLE);
+        frontAniSet.start();
+        Thread thread=new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        cardFront.setVisibility(View.INVISIBLE);
+                    }
+                }
+        );
+        //排队等待cpu
+        thread.start();
+
+        backAniSet.start();
+
+
+
+    }
+
+    //广播接收器
     public void registerMyBroadcastReceiver(){
         IntentFilter intentFilter=new IntentFilter();
+        //添加action，标记广播接收范围
         intentFilter.addAction("Counter");
         registerReceiver(myBroadcastReceiver,intentFilter);
     }
--- app/src/main/java/cn/kahvia/adoing/service/CounterService.java
+++ app/src/main/java/cn/kahvia/adoing/service/CounterService.java
@@ -18,34 +18,40 @@ public class CounterService extends Service {
     public static int counter=0;
     public static int pageIndex=-1;
     public static Thread counterThread=null;
-    //多线程是否进行的标准。
-    public static boolean running=true;
+    //子线程是否继续进行的标准。
+    public static boolean running=false;
+    public static boolean threadEnd=true;
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         pageIndex=Integer.parseInt(intent.getStringExtra("pageIndex"));
         if (intent.getAction().equals("start")){
             running=true;
+            threadEnd=false;
             counterThread=new Thread(
                     new Runnable() {
                         @Override
                         public void run() {
-
                             while (running){
                                 try {
+                                    Thread.sleep(1000);
                                     counter++;
                                     sendMessageToActivity(counter);
-                                    Log.i("Counter",counter+"");
-                                    Thread.sleep(1000);
+//                                    Log.i("Counter",counter+"");
                                 } catch (InterruptedException e) {
                                     e.printStackTrace();
                                 }
                             }
+                            //计时器停止工作后，计时器归零
+                            counter=0;
+                            //确认不会对计时器的数值进行操作后，标志线程终止
+                            threadEnd=true;
                         }
                     }
             );
             counterThread.start();
             final String CHANNELID="TaskCounter";
+            //定义通知栏通知面板
             NotificationChannel channel=new NotificationChannel(
                     CHANNELID,
                     CHANNELID,
@@ -58,8 +64,9 @@ public class CounterService extends Service {
                     .setSmallIcon(R.drawable.notification);
             startForeground(666,notification.build());
         }else {
-            counter=0;
+            //设置running为false，以结束线程上的计数循环
             running=false;
+            //停止前台并移除通知
             stopForeground(true);
             stopSelf();
         }
@@ -73,6 +80,7 @@ public class CounterService extends Service {
     }
 
     private void sendMessageToActivity(int counter) {
+        //发送广播，传递计时器的数据
         Intent intent = new Intent("Counter");
         intent.putExtra("Counter", counter);
         sendBroadcast(intent);
